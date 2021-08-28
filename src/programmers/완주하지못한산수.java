56771cfb917f05f0aef303302b8a8a8fd2476539
hoki8823 <hoki8823@naver.com>
2021-08-28 21:33:56 +0900

    hash

---
+++ src/programmers/완주하지못한산수.java
@@ -0,0 +1,27 @@
+package programmers;
+import java.util.HashMap;
+
+public class Question_1 {
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        // HashMap<key ,value>
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        //동명이인 처리
+        for (String player : participant) 
+            hm.put(player, hm.getOrDefault(player,0) + 1);
+        
+        //완주한사람 value값 0처리
+        for (String player : completion) 
+            hm.put(player, hm.get(player) - 1);
+        
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/programmers/전화번호목록.java
@@ -0,0 +1,34 @@
+package programmers;
+import java.util.HashMap;
+
+public class Question_2 {
+	public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        //모든 전화번호 해싱
+        for(int i=0;i<phone_book.length;i++)
+        	hm.put(phone_book[i], i);
+        
+        //접두어 유무확인
+        for(int i=0;i<phone_book.length;i++) {
+        	for(int j=1;j<phone_book[i].length();j++) {
+        		if(hm.containsKey(phone_book[i].substring(0, j))) {
+        			answer = false;
+        			break;
+        		}
+        	}
+        }
+        return answer;
+	}
+}
+
+/*
+startsWith(문자열) - 특정 문자열로 시작하는지 체크
+endsWith(문자열) - 특정 문자열로 끝나는지 체크
+
+hashmap.containsKey(key) - 맵에서 인자로 보낸 키가 있으면 true 없으면 false
+hashmap.containsValue(value) - 맵에서 인자로 보낸 값이 있으면 true 없으면 false
+
+*/
