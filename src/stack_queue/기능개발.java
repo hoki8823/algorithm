2bc3f3f538b6e251a773b2e653f8954b2d596f9e
hoki8823 <hoki8823@naver.com>
2021-09-01 19:16:51 +0900

    stack,queue

---
+++ src/stack_queue/기능개발.java
@@ -0,0 +1,18 @@
+package stack_queue;
+
+import java.util.*;
+
+public class 기능개발 {
+	public int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day=-1;
+        for(int i=0;i<progresses.length;i++) {
+        	while(progresses[i] + (speeds[i] * day) < 100) {
+        		day++;
+        	}
+        	dayOfend[day]++;
+        }
+        
+        return Arrays.stream(dayOfend).filter(i -> i != 0).toArray();
+    }
+}
